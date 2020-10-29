ff674134afae2465372398e3c3239858d40ff889
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-10-29 15:25:21 +0530

    XAMARIN-37313 - Sample and screenshot added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Model/ModelClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class Cities
+    {
+        public string Name { get; set; }
+    }
+    public class States
+    {
+        public List<Cities> Cities { get; set; }
+        public string Name { get; set; }
+    }
+    public class Countries
+    {
+        public List<States> States { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/ViewModel/TreeViewViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class TreeViewViewModel
+    {
+        #region Fields
+        private ObservableCollection<Countries> imageNodeInfo;
+        #endregion
+        #region Constructor
+        public TreeViewViewModel()
+        {
+            GenerateSource();
+        }
+        #endregion
+        #region Properties
+        public ObservableCollection<Countries> ImageNodeInfo
+        {
+            get { return imageNodeInfo; }
+            set { this.imageNodeInfo = value; }
+        }
+        #endregion
+        #region Generate Source
+        private void GenerateSource()
+        {
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("TreeViewXamarin.Data.navigation.json");
+            using (StreamReader sr = new StreamReader(stream))
+            {
+                var jsonText = sr.ReadToEnd();
+                ImageNodeInfo = new ObservableCollection<Countries>();
+                var MyArrayData = JsonConvert.DeserializeObject<List<Countries>>(jsonText);
+                foreach (var data in MyArrayData)
+                {
+                    ImageNodeInfo.Add(data);
+                }
+            }
+        }
+        #endregion
+    }
+}
